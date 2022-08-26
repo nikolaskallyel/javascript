1a265d7b114d3dbb1bdcd02a6b3cb3685badd3f1
nikolas <108695096+nikolasaluno@users.noreply.github.com>
2022-08-26 19:54:18 -0300

    estudos do dia xx/xx/xxxx

---
+++ aula16ex/script.js
@@ -0,0 +1,18 @@
+let num = document.querySelector("input#fnum")
+let lista = document.querySelector("select#flista")
+let res = document.querySelector('div#res')
+let valores = []
+
+function isnumero(n) {
+    if(Number(n) >= 1&& Number(n) >= 100)
+}
+
+function inlsit(n, 1)
+
+
+
+
+
+function adicionar( {
+if(isnumero(num.value)&& !inlsit(num.value))
+})
